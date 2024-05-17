0c2d593c02371ba24d2bd8ed0495d4050bda2bda
Sarvarian <Sarvarian@Hotmail.com>
2024-05-17 17:23:16 +0330

    Create AbanDotAbDidNotFound.cs

---
+++ AbanLib/LogClasses/AbanDotAbDidNotFound.cs
@@ -0,0 +1,20 @@
+﻿using AbanLib.Logging;
+
+namespace AbanLib.LogClasses;
+
+public class AbanDotAbDidNotFound(string path) : LogBase
+{
+	public override ILogElement[] Elements =>
+	[
+		new LogElementString(nameof(path), path)
+	];
+
+	public override string Title => "Failed to find 'aban.ab'.";
+	public override string Message => $"Could not locate any 'aban.ab' file in directory {path}";
+
+	public override string Formatted =>
+		$"{Title}\n{Message}"; // TODO: Move Formatted and FormattedVerbose to LogBase to get generated base on Title and Message and Elements.
+
+	public override string FormattedVerbose =>
+		$"{Formatted}\n..."; // TODO: Write a generator for this in LogBase and before that write a test for the generator routine. 
+}
