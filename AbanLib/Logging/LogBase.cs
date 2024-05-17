96f714b9612854f0c244385b509c1f2f9936368d
Sarvarian <Sarvarian@Hotmail.com>
2024-05-17 10:36:51 +0330

    Update LogBase.cs

--- AbanLib/Logging/LogBase.cs
+++ AbanLib/Logging/LogBase.cs
@@ -1,15 +1,20 @@
 ﻿namespace AbanLib.Logging;
 
-public abstract class LogBase : ILog
+public abstract class LogBase(DateTime logTime) : ILog
 {
-	public abstract DateTime Time { get; }
+	protected LogBase() : this(DateTime.Now)
+	{
+	}
+
+	public DateTime Time { get; init; } = logTime;
+	public ILog? PreviousLog { get; protected set; } = null;
+	public ILog? NextLog { get; protected set; } = null;
+
 	public abstract string Title { get; }
 	public abstract string Message { get; }
 	public abstract string Formatted { get; }
 	public abstract string FormattedVerbose { get; }
 	public abstract ILogElement[] Elements { get; }
 
-	public ILog? PreviousLog { get; } = null;
-	public ILog? NextLog { get; } = null;
-	public object? ExtraDataForUser { get; set; } = null;
+	object? ILog.ExtraDataForUser { get; set; } = null;
 }
