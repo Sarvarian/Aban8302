b15b5ce500872626ddc3f07442a79bf4995a7f58
Sarvarian <Sarvarian@Hotmail.com>
2024-05-18 05:14:17 +0330

    Create LogElementTest.cs

---
+++ AbanLibTests/Logging/LogElementTest.cs
@@ -0,0 +1,15 @@
+﻿using AbanLib.Logging;
+using AbanLibTests.facility;
+
+namespace AbanLibTests.Logging;
+
+public class LogElementTest : Test
+{
+	[Fact]
+	public void EnsureGeneratorAmountIsConsidersAllTheSubTypesOfILogElement()
+	{
+		// Minus 1 to discount the ILogElement itself from the count.
+		var shouldBe = SubTypesOf<ILogElement>().Length - 1;
+		Assert.Equal(shouldBe, ILogElement.Generators.Length);
+	}
+}
