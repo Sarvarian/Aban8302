68b5a65588056cbf83b57051a14e6591f8d32f44
Sarvarian <Sarvarian@Hotmail.com>
2024-05-18 11:06:38 +0330

    Update AbanDotAbDidNotFound.cs

--- AbanLib/LogClasses/AbanDotAbDidNotFound.cs
+++ AbanLib/LogClasses/AbanDotAbDidNotFound.cs
@@ -11,10 +11,4 @@ public class AbanDotAbDidNotFound(string path) : LogBase
 
 	public override string Title => "Failed to find 'aban.ab'.";
 	public override string Message => $"Could not locate any 'aban.ab' file in directory {path}";
-
-	public override string Formatted =>
-		$"{Title}\n{Message}"; // TODO: Move Formatted and FormattedVerbose to LogBase to get generated base on Title and Message and Elements.
-
-	public override string FormattedVerbose =>
-		$"{Formatted}\n..."; // TODO: Write a generator for this in LogBase and before that write a test for the generator routine. 
 }
