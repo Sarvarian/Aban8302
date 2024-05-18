f78da16e03196d0f59071ae7d5d184121b87544e
Sarvarian <Sarvarian@Hotmail.com>
2024-05-18 05:10:38 +0330

    Update TestTest.cs

--- AbanLibTests/facility/TestTest.cs
+++ AbanLibTests/facility/TestTest.cs
@@ -1,8 +1,6 @@
-﻿using Xunit.Abstractions;
+﻿namespace AbanLibTests.facility;
 
-namespace AbanLibTests.facility;
-
-public class TestTest(ITestOutputHelper output) : Test
+public class TestTest : Test
 {
 	[Fact]
 	public void CreatesTempDirectoryOnConstruction()
@@ -16,37 +14,5 @@ public class TestTest(ITestOutputHelper output) : Test
 		base.Dispose();
 		GC.SuppressFinalize(this);
 		Assert.False(Path.Exists(DirectoryPath));
-	}
-
-	[Fact]
-	public void GenerateGuid()
-	{
-		Guid.Parse(GuidString);
-	}
-
-	[Fact]
-	public void MakeSureGuidsAreDifferent1()
-	{
-		output.WriteLine(DirectoryName);
-	}
-
-	[Fact]
-	public void MakeSureGuidsAreDifferent2()
-	{
-		output.WriteLine(DirectoryName);
-	}
-
-	[Fact]
-	public void GuidStringGivesNewGuidEachTime()
-	{
-		var g1 = GuidString;
-		var g2 = GuidString;
-		var g3 = GuidString;
-		Assert.NotEqual(g1, g2);
-		Assert.NotEqual(g2, g3);
-		Assert.NotEqual(g3, g1);
-		Assert.NotEqual(GuidString, GuidString);
-		Assert.NotEqual(GuidString, GuidString);
-		Assert.NotEqual(GuidString, GuidString);
 	}
 }
