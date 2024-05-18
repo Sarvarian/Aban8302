1b65a5f55a4cbc855a9435f95f7507ade17cf876
Sarvarian <Sarvarian@Hotmail.com>
2024-05-18 11:00:31 +0330

    Update RandomLog.cs

--- AbanLibTests/facility/RandomLog.cs
+++ AbanLibTests/facility/RandomLog.cs
@@ -5,14 +5,33 @@ namespace AbanLibTests.facility;
 
 public class RandomLog : LogBase
 {
-	public override ILogElement[] Elements { get; } = GenerateRandomElements();
+	private RandomLog()
+	{
+	}
+
+	private RandomLog(ILogElement[] elements)
+	{
+		Elements = elements;
+	}
+
+	public override ILogElement[] Elements { get; } = GenerateRandomElements((uint)Rand.Random.Next(10, 100));
 	public override string Title { get; } = GenerateRandomTitle();
 	public override string Message { get; } = GenerateRandomMessage();
 
-	private static ILogElement[] GenerateRandomElements()
+	public static ILog Create()
+	{
+		return new RandomLog();
+	}
+
+	public static ILog Create(ILogElement[] elements)
+	{
+		return new RandomLog(elements);
+	}
+
+	public static ILogElement[] GenerateRandomElements(uint howMany)
 	{
 		var elements = new List<ILogElement>();
-		for (var i = 0; i < Rand.Random.Next(10, 100); i++) elements.Add(ILogElement.GenerateRandom());
+		for (uint i = 0; i < howMany; i++) elements.Add(ILogElement.GenerateRandom());
 		return elements.ToArray();
 	}
 
